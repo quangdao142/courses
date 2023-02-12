7b00ccb7d7d00005d5686a0c417ed50035818029
Quang Dao <quang1425valentine@gmail.com>
2023-02-12 20:57:48 +0700

    commit

---
+++ src/config/db/index.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+async function connect(){
+
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/education_dev',{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Connect Successfully');
+    } catch (error) {
+        console.log('Connect Failure');
+    }
+
+}
+
+
+module.exports = {connect};
---
+++ src/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const path = require('path');
+
+const route = require('./routes');
+const db = require('./config/db');
+
+// Connect to db
+db.connect();
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+// HTTP logger
+// app.use(morgan('combined'));
+
+// template engine
+app.engine('hbs', handlebars.engine({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
+
+// routes init
+
+route(app);
+
+
+
+// 127.0.0.1 - localhost
+app.listen(port, () => console.log('App listening at port',port));
