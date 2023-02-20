343d55cd9f6e45f2f8fe0c8155e87ec418a24fe6
truong-vx352 <truonglong352000@gmail.com>
2023-02-20 23:58:36 +0700

    add search

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,27 +1,37 @@
-const Course = require('../models/Course');
-const { multipleMongooseToObject } = require('../../util/mongoose');
+const Course = require("../models/Course");
+const { multipleMongooseToObject } = require("../../util/mongoose");
 
-class SiteController{
+class SiteController {
+	// [GET] /
+	index(req, res, next) {
+		Course.find({}).then((courses) => {
+			res.render("home", {
+				courses: multipleMongooseToObject(courses),
+			});
+		});
 
-    // [GET] /
-    index(req, res, next){
-        
-        Course.find({})
-            .then(courses => {
-                res.render('home', {
-                    courses: multipleMongooseToObject(courses),                  
-                })
-            })
-            .catch(next);
-        
-        // res.render('home');
-    }
+		// res.render('home');
+	}
 
-    // [GET] /search
-    search(req, res){
-        res.render('search');
-    }
-
+	search(req, res, next) {
+		res.render("search");
+	}
+	// [GET] /search
+	searchById(req, res, next) {
+		Course.find({ $text: { $search: req.query.name || "" } }).then((result) => {
+			if (result.length === 0) {
+				Course.find({}).then((courses) => {
+					res.render("home", {
+						courses: multipleMongooseToObject(courses),
+					});
+				});
+			} else {
+				return res.render("home", {
+					courses: multipleMongooseToObject(result),
+				});
+			}
+		});
+	}
 }
 
-module.exports = new SiteController;
+module.exports = new SiteController();
--- src/routes/site.js
+++ src/routes/site.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const siteController = require('../app/controllers/SiteController');
 
 router.get('/search', siteController.search);
-router.get('/', siteController.index);
+// router.get('/', siteController.index);
+router.get('/:name', siteController.searchById);
+router.get('/', siteController.searchById);
 
     
 module.exports = router;
